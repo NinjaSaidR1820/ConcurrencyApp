382e739272cbdfdd35e110e8c92afadef40da30a
SaidRivas <89547815+NinjaSaidR1820@users.noreply.github.com>
2022-04-20 09:50:42 -0600

    Add files via upload

---
+++ ConcurrencyApp/ConcurrencyApp/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace ConcurrencyApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.ptnStop = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pgbStatus = new System.Windows.Forms.ProgressBar();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(228, 141);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(347, 141);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 1;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.BtnPause_Click);
+            // 
+            // ptnStop
+            // 
+            this.ptnStop.Location = new System.Drawing.Point(466, 141);
+            this.ptnStop.Name = "ptnStop";
+            this.ptnStop.Size = new System.Drawing.Size(75, 23);
+            this.ptnStop.TabIndex = 2;
+            this.ptnStop.Text = "Stop";
+            this.ptnStop.UseVisualStyleBackColor = true;
+            this.ptnStop.Click += new System.EventHandler(this.PtnStop_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.pgbStatus);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(29, 239);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(748, 33);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // pgbStatus
+            // 
+            this.pgbStatus.Location = new System.Drawing.Point(3, 3);
+            this.pgbStatus.Name = "pgbStatus";
+            this.pgbStatus.Size = new System.Drawing.Size(738, 23);
+            this.pgbStatus.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.ptnStop);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Progress";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button ptnStop;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.ProgressBar pgbStatus;
+    }
+}
+
---
+++ ConcurrencyApp/ConcurrencyApp/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConcurrencyApp
+{
+    public partial class Form1 : Form
+    {
+
+        private delegate void SetProgressBarValueEvent(int value);
+        private bool completed = false;
+        private int i = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            if(completed)
+            {
+                completed = false;
+            }
+
+
+            Thread t = new Thread(new ThreadStart(
+                FillProgressBar
+                ));
+
+
+            t.Start();
+        }
+
+        public void FillProgressBar()
+        {
+            while (!completed && i <= 100)
+            {               
+                RequiredInvoke(i++);
+                Thread.Sleep(100);
+            }
+
+        }
+        public void RequiredInvoke(int value)
+        {
+            if (pgbStatus.InvokeRequired)
+            {
+                SetProgressBarValueEvent progressBarValueEvent = new SetProgressBarValueEvent(SetProgressBarValue);
+                BeginInvoke(progressBarValueEvent, new object[] { value });
+            }
+            else
+            {
+                SetProgressBarValue(value);
+            }
+        }
+
+        public void SetProgressBarValue(int value)
+        {
+            
+            pgbStatus.Value = value;
+            
+        }
+
+        private void BtnPause_Click(object sender, EventArgs e)
+        {
+            completed = true;
+        }
+
+        private void PtnStop_Click(object sender, EventArgs e)
+        {
+            pgbStatus.Value = 0;
+            completed = true;
+        }
+    }
+}
